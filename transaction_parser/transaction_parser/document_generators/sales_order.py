71de310220b385878973b5b94056ffd4cc1df349
aayush7908 <aayush.dalal4314@gmail.com>
2025-03-21 19:07:38 +0530

    fix: get item code for sales order

--- transaction_parser/transaction_parser/document_generators/sales_order.py
+++ transaction_parser/transaction_parser/document_generators/sales_order.py
@@ -7,14 +7,17 @@ from transaction_parser.transaction_parser.document_generators.transaction impor
 
 class SalesOrderGenerator(TransactionGenerator):
     DOCTYPE = "Sales Order"
+    PARTY_DOCTYPE = "Customer"
 
     def set_details(self):
-        company_name = self.get_company()
-
-        if not company_name:
-            # Notification with link to error log
-            raise ValueError(f"Company {self.parsed_data.company} not found")
-
-    ### Company
-    def get_company(self, parsed_company_name):
         pass
+
+    def get_item_code(self, item):
+        return frappe.db.get_value(
+            "Item Customer Detail",
+            filters={
+                "parenttype": "Item",
+                "ref_code": item.party_item_code,
+            },
+            fieldname="parent",
+        )
