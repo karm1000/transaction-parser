7d1a7ffe6939ed9b4b174e42fce2316d576f8f3c
aayush7908 <aayush.dalal4314@gmail.com>
2025-03-21 18:56:33 +0530

    fix: implement core logic to get items

--- transaction_parser/transaction_parser/document_generators/transaction.py
+++ transaction_parser/transaction_parser/document_generators/transaction.py
@@ -14,13 +14,11 @@ class TransactionGenerator:
 
         self.parsed_data = None
         self.doc = None
-
         self.is_company_party_inverted = False
+
         self.companies = None
         self.parties = None
         self.currencies = None
-        self.item_names = None
-        self.uoms = None
 
     def generate(self, parsed_data):
         self.parsed_data = parsed_data
@@ -149,71 +147,40 @@ class TransactionGenerator:
         return self.currencies
 
     ### Items
-    def get_items(self):
-        return [self.get_item(parsed_item) for parsed_item in self.parsed_data.items]
+    def get_items(self, items, company=None, currency=None):
+        return [self.get_item(item, company, currency) for item in items]
 
-    def get_item(self, parsed_item):
-        # TODO: Implement
-        item = frappe._dict()
+    def get_item(self, item, company, currency):
+        _item = frappe._dict()
 
-        # item.item_name = self.guess_item_name(parsed_item.description)
-        item.item_code = self.get_item_code(item.item_name)
-        # item.uom = self.guess_item_uom(parsed_item.unit)
-        # TODO: customer item code
-        item.qty = parsed_item.quantity
-        item.rate = parsed_item.rate
-        item.amount = parsed_item.amount
-
-        # rate company currency
-        # amount company currency
-        # net rate
-        # net amount
-        # net rate company currency
-        # net amount company currency
-        # item tax template
+        _item.qty = item.quantity
+        _item.rate = item.rate
+        _item.amount = item.amount
+        _item.party_item_code = item.party_item_code
+        _item.item_code = self.get_item_code(_item)
 
         return {
-            **self._get_item_details(item),
-            **item,
+            **self._get_item_details(_item, company, currency),
+            **_item,
         }
 
-    ### Item Name
-    def guess_item_name(self, parsed_item_name):
-        return self.guess_value(parsed_item_name, self._get_item_names())
-
-    def _get_item_names(self):
-        if not self.item_names:
-            self.item_names = frappe.db.get_all("Item", pluck="item_name")
-
-        return self.item_names
-
     ### Item Code
-    def get_item_code(self, item_name):
-        return frappe.db.get_value(
-            "Item",
-            filters={"item_name": item_name},
-            fieldname="name",
-        )
-
-    ### Item UOM
-    def guess_item_uom(self, parsed_uom):
-        return self.guess_value(parsed_uom, self._get_uoms())
-
-    def _get_uoms(self):
-        if not self.uoms:
-            self.uoms = frappe.db.get_all("UOM", pluck="name")
-
-        return self.uoms
+    def get_item_code(self, item):
+        # TODO: Implement
+        pass
 
     ### Item Details from ERP
-    def _get_item_details(self, item):
+    def _get_item_details(self, item, company, currency):
+        if not (item.item_code and company and currency):
+            return {}
+
         return get_item_details(
             {
                 "item_code": item.item_code,
                 "qty": item.qty,
                 "rate": item.rate,
-                "company": self.doc.company,
-                "currency": self.doc.currency,
+                "company": company,
+                "currency": currency,
                 "doctype": self.DOCTYPE,
             }
         )
