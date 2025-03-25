c7e3e0dd4d38dabb00b7ec35804d9bf7a427b04d
aayush7908 <aayush.dalal4314@gmail.com>
2025-03-25 16:08:26 +0530

    feat: get party by gstin and pan for India

--- transaction_parser/transaction_parser/regional_overrides/india/document_generators/transaction.py
+++ transaction_parser/transaction_parser/regional_overrides/india/document_generators/transaction.py
@@ -19,7 +19,10 @@ class IndiaTransactionGenerator(TransactionGenerator):
 
         self.company_gstins = None
         self.company_pans = None
+        self.party_gstins = None
+        self.party_pans = None
 
+    ### Company
     def search_company(self, company):
         if found := self.search_company_by_gstin(company.gstin):
             return found
@@ -30,6 +33,9 @@ class IndiaTransactionGenerator(TransactionGenerator):
         return super().search_company(company)
 
     def search_company_by_gstin(self, gstin):
+        return self.search_business_by_gstin(gstin, "Company")
+
+    def search_business_by_gstin(self, gstin, doctype):
         from india_compliance.gst_india.utils import get_party_for_gstin, validate_gstin
 
         if not gstin:
@@ -37,19 +43,22 @@ class IndiaTransactionGenerator(TransactionGenerator):
 
         try:
             if validate_gstin(gstin):
-                return get_party_for_gstin(gstin, "Company")
+                return get_party_for_gstin(gstin, doctype)
 
         except Exception:
             return
 
     def search_company_by_pan(self, pan):
+        return self.search_business_by_pan(pan, "Company")
+
+    def search_business_by_pan(self, pan, doctype):
         from india_compliance.gst_india.utils import is_valid_pan
 
         if not pan:
             return
 
         if is_valid_pan(pan):
-            return frappe.db.get_value("Company", {"pan": pan}, fieldname="name")
+            return frappe.db.get_value(doctype, {"pan": pan}, fieldname="name")
 
     def guess_company(self, company):
         if found := self.guess_company_by_gstin(company.gstin):
@@ -99,3 +108,48 @@ class IndiaTransactionGenerator(TransactionGenerator):
                 as_list=True,
             )
         )
+
+    ### Party
+    def search_party(self, party):
+        if found := self.search_party_by_gstin(party.gstin):
+            return found
+
+        if found := self.search_party_by_pan(party.pan):
+            return found
+
+        return super().search_party(party)
+
+    def search_party_by_gstin(self, gstin):
+        return self.search_business_by_gstin(gstin, self.PARTY_DOCTYPE)
+
+    def search_party_by_pan(self, pan):
+        return self.search_business_by_pan(pan, self.PARTY_DOCTYPE)
+
+    def guess_party(self, party):
+        if found := self.guess_party_by_gstin(party.gstin):
+            return found
+
+        if found := self.guess_party_by_pan(party.pan):
+            return found
+
+        return super().guess_party(party)
+
+    def guess_party_by_gstin(self, gstin):
+        if found := self.guess_value(gstin, self._get_all_party_gstins()):
+            return self._get_all_party_gstins().get(found)
+
+    def _get_all_party_gstins(self):
+        if not self.party_gstins:
+            self.party_gstins = self._get_all_gstins(self.PARTY_DOCTYPE)
+
+        return self.party_gstins
+
+    def guess_party_by_pan(self, pan):
+        if found := self.guess_value(pan, self._get_all_party_pans()):
+            return self._get_all_party_pans().get(found)
+
+    def _get_all_party_pans(self):
+        if not self.party_pans:
+            self.party_pans = self._get_all_pans(self.PARTY_DOCTYPE)
+
+        return self.party_pans
