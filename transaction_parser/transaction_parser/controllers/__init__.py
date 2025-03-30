ea64eff08956b2454276403058428103c9d5f016
aayush7908 <aayush.dalal4314@gmail.com>
2025-03-30 18:36:59 +0530

    fix: support adding hooks for base controllers

--- transaction_parser/transaction_parser/controllers/__init__.py
+++ transaction_parser/transaction_parser/controllers/__init__.py
@@ -13,7 +13,7 @@ def get_controller(country, doctype):
     if (country != "Other") and (controller := _get_controller("Other", doctype)):
         return controller
 
-    if controller := _get_default_controller(doctype):
+    if controller := _get_base_controller(doctype):
         return controller
 
     frappe.throw(_(f"No controller found for {doctype}"))
@@ -36,6 +36,25 @@ def _get_controller_from_hooks(country, doctype):
 
     transaction_parser_overrides = {
         "India": {
+            "Transaction": [
+                "path.to.controller.ClassName",
+            ],
+            "Sales Order": [
+                "path.to.controller.ClassName",
+            ]
+        }
+        "Other": {
+            "Transaction": [
+                "path.to.controller.ClassName",
+            ],
+            "Sales Order": [
+                "path.to.controller.ClassName",
+            ]
+        }
+        "Base": {
+            "Transaction": [
+                "path.to.controller.ClassName",
+            ],
             "Sales Order": [
                 "path.to.controller.ClassName",
             ]
@@ -71,7 +90,10 @@ def _get_attr(path):
         return
 
 
-def _get_default_controller(doctype):
+def _get_base_controller(doctype):
+    if found := _get_controller_from_hooks("Base", doctype):
+        return found
+
     _module = frappe.scrub(doctype)
     _class = _get_class_name(doctype)
 
