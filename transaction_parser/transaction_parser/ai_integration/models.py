fdea818c4c28671ccaba329b4b613fe1cdac14cb
aayush7908 <aayush.dalal4314@gmail.com>
2025-04-01 13:37:33 +0530

    feat: support OpenAI gpt-4o

--- transaction_parser/transaction_parser/ai_integration/models.py
+++ transaction_parser/transaction_parser/ai_integration/models.py
@@ -13,6 +13,9 @@ class Model:
     response_format = None
 
 
+### deepseek
+
+
 class DeepSeek(Model):
     service_provider = "DeepSeek"
     base_url = "https://api.deepseek.com"
@@ -28,7 +31,23 @@ class DeepSeekReasoner(DeepSeek):
     response_format = ResponseFormat.TEXT.value
 
 
+### openai
+
+
+class OpenAI(Model):
+    service_provider = "OpenAI"
+    base_url = "https://api.openai.com/v1"
+    response_format = ResponseFormat.JSON.value
+
+
+class OpenAIGPT4O(OpenAI):
+    name = "gpt-4o"
+
+
+### model-class mapping
+
 MODELS = {
     "DeepSeek Chat": DeepSeekChat,
     "DeepSeek Reasoner": DeepSeekReasoner,
+    "OpenAI gpt-4o": OpenAIGPT4O,
 }
