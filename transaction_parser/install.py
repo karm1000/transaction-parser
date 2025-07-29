3ea6a8e29424fe576dfd8b98a24eeec6f8fe60ea
priyanshshah2442 <priyanshshah2442@gmail.com>
2025-07-29 12:27:47 +0530

    fix: update Parser Settings for extending functionality

--- transaction_parser/install.py
+++ transaction_parser/install.py
@@ -1,8 +1,9 @@
+import frappe
 from frappe.custom.doctype.custom_field.custom_field import (
     create_custom_fields,
 )
 
-custom_fields = {
+CUSTOM_FIELDS = {
     "Sales Order": [
         {
             "fieldname": "is_created_by_transaction_parser",
@@ -23,6 +24,20 @@ custom_fields = {
     ],
 }
 
+INDIA_SPECIFIC_CUSTOM_FIELDS = {
+    "Transaction Parser Settings": [
+        {
+            "fieldname": "in_auto_create_supplier",
+            "fieldtype": "Check",
+            "label": "Auto Create Supplier",
+            "insert_after": "auto_create_items",
+            "description": "Automatically create a supplier based on GSTIN from the invoice",
+        }
+    ]
+}
+
 
 def after_install():
-    create_custom_fields(custom_fields)
+    create_custom_fields(CUSTOM_FIELDS)
+    if "india_compliance" in frappe.get_installed_apps():
+        create_custom_fields(INDIA_SPECIFIC_CUSTOM_FIELDS)
