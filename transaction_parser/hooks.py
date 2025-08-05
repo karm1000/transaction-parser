fad50fd292bc8b2ad0b3f4240344d0de2e0a468c
priyanshshah2442 <priyanshshah2442@gmail.com>
2025-08-05 17:58:17 +0530

    fix: common utility for Parsing transaction from list view

--- transaction_parser/hooks.py
+++ transaction_parser/hooks.py
@@ -13,6 +13,7 @@ app_include_js = "transaction_parser.bundle.js"
 
 doctype_list_js = {
     "Sales Order": "transaction_parser/client_scripts/sales_order_list.js",
+    "Purchase Invoice": "transaction_parser/client_scripts/purchase_invoice_list.js",
 }
 
 doc_events = {
--- transaction_parser/transaction_parser/client_scripts/purchase_invoice_list.js
+++ transaction_parser/transaction_parser/client_scripts/purchase_invoice_list.js
@@ -1,6 +1,6 @@
-const DOCTYPE = "Sales Order";
+const DOCTYPE = "Purchase Invoice";
 frappe.require("assets/transaction_parser/js/transaction_parser_dialog.js");
 
 frappe.listview_settings[DOCTYPE].onload = async function (list_view) {
-	create_transaction_parser_dialog(DOCTYPE, list_view); // eslint-disable-line no-undef
+	create_transaction_parser_dialog("Expense", list_view); // eslint-disable-line no-undef
 };
