e14016fb4a7a152186b8f299daf2c0c1ef25404f
priyanshshah2442 <priyanshshah2442@gmail.com>
2025-09-02 17:28:03 +0530

    fix: fetch item price if not present in PDF

--- transaction_parser/transaction_parser/controllers/sales_order.py
+++ transaction_parser/transaction_parser/controllers/sales_order.py
@@ -203,8 +203,13 @@ class SalesOrder(Transaction):
             )
         )
 
+        price_list = frappe.db.get_value(
+            "Customer", {"name": self.doc.customer}, "default_price_list"
+        )
         items = [
-            self.get_item(item, item_codes.get(item.party_item_code))
+            self.get_item(
+                item, item_codes.get(item.party_item_code), price_list=price_list
+            )
             for item in self.data.item_list
         ]
 
