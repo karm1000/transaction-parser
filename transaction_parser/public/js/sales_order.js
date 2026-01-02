a51e016815d8ac090c8126081a94c3cd86dae0ce
Karm Soni <karmdsoni8159@gmail.com>
2026-01-02 13:53:56 +0530

    fix: do not apply price list

--- transaction_parser/public/js/sales_order.js
+++ transaction_parser/public/js/sales_order.js
@@ -1,30 +1,11 @@
 frappe.ui.form.on("Sales Order", {
-	customer(frm) {
+	setup(frm) {
 		if (!frm.doc.is_created_by_transaction_parser) {
 			return;
 		}
 
-		for (const item of frm.doc.items || []) {
-			item.__old_rate = item.rate;
-		}
-	},
-});
-
-frappe.ui.form.on("Sales Order Item", {
-	rate(frm, cdt, cdn) {
-		if (!frm.doc.is_created_by_transaction_parser) {
-			return;
-		}
-
-		const item = frappe.get_doc(cdt, cdn);
-
-		const old_rate = item.__old_rate;
-		item.__old_rate = undefined;
-
-		if (old_rate === undefined || old_rate === item.rate) {
-			return;
-		}
-
-		frappe.model.set_value(cdt, cdn, "rate", old_rate);
+		frm.cscript.customer = function () {
+			erpnext.utils.get_party_details(this.frm, null, null);
+		};
 	},
 });
