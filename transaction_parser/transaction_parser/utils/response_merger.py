9e130906fc15b06599ad60ce17cdc577362d003c
Karm Soni <karmdsoni8159@gmail.com>
2026-04-06 18:16:30 +0530

    fix: update condition in _merge_primitive to check for None before assigning source_value

--- transaction_parser/transaction_parser/utils/response_merger.py
+++ transaction_parser/transaction_parser/utils/response_merger.py
@@ -156,7 +156,7 @@ class ResponseMerger:
                 self._merge_list(field_type, target, key, source_value)
 
     def _merge_primitive(self, target: dict, key: str, source_value: Any) -> None:
-        if not target.get(key) and source_value:
+        if target.get(key) is None and source_value:
             target[key] = source_value
 
     def _merge_object(
