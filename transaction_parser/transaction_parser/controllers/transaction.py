3500c318e2745715fa2c6b503a38a339d14d17af
Abdeali Chharchhoda <abdealiking786@gmail.com>
2026-04-15 18:11:35 +0530

    fix: add validation to ensure at least one file is attached for parsing

--- transaction_parser/transaction_parser/controllers/transaction.py
+++ transaction_parser/transaction_parser/controllers/transaction.py
@@ -2,6 +2,7 @@ import erpnext
 import frappe
 from erpnext.setup.utils import get_exchange_rate
 from erpnext.stock.get_item_details import get_item_details
+from frappe import _
 from rapidfuzz import fuzz, process
 
 from transaction_parser.transaction_parser.ai_integration.parser import AIParser
@@ -37,6 +38,12 @@ class Transaction:
         ai_model: str | None = None,
         page_limit: int | None = None,
     ):
+        if not files:
+            frappe.throw(
+                title=_("No files attached"),
+                message=_("Please attach at least one file to parse"),
+            )
+
         self.initialize()
 
         self.files = files
@@ -320,9 +327,7 @@ class Transaction:
         self.doc.flags.ignore_links = True
 
     def _attach_file(self) -> None:
-        files_to_attach = self.files if isinstance(self.files, list) else [self.files]
-
-        for file_doc in files_to_attach:
+        for file_doc in self.files:
             file_doc.attached_to_doctype = self.DOCTYPE
             file_doc.attached_to_name = self.doc.name
             file_doc.save()
