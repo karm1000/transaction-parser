d3839e5f9afbe9a8f8281cc999fbaa96cb8cab17
Karm Soni <karmdsoni8159@gmail.com>
2026-05-25 13:02:41 +0530

    fix: use model_dump_json instead of converting response to dict and again json

--- transaction_parser/transaction_parser/ai_integration/parser.py
+++ transaction_parser/transaction_parser/ai_integration/parser.py
@@ -98,7 +98,9 @@ class AIParser:
             response = self._make_api_call(request_params)
             log.request_id = response.id
 
-            response_dict = to_dict(json.dumps(response.to_dict()))
+            response_dict = json.loads(
+                response.model_dump_json(), object_hook=frappe._dict
+            )
             log.output = response_dict
 
             processed_response = self._process_response(response_dict)
